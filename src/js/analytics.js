268ecaf501fba48fa8b919ad68f5b24273dd05c5
Brent Jones <bjones@npr.org>
2023-11-30 15:55:35 -0600

    update per interactive template (nprapps/interactive-template@0c1d9fd)

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,12 +1,11 @@
-var DataConsent = require('./lib/data-consent');
 
+
+var DataConsent = require('./lib/data-consent');
 var googleAnalyticsAlreadyInitialized = false;
 
 var setupGoogleAnalytics = function() {
-  // Bail early if opted out of Performance and Analytics consent groups
-  if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) return;
-
-
+  	// Bail early if opted out of Performance and Analytics consent groups
+  	if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) return;
 
 
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -14,7 +13,7 @@ var setupGoogleAnalytics = function() {
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	if (window.top !== window) {  
+	if (window.top !== window) { 
 
 		ga("create", "UA-5828686-75", "auto");
 		// By default Google tracks the query string, but we want to ignore it.
@@ -38,41 +37,19 @@ var setupGoogleAnalytics = function() {
 		  dimension2: parentHostname,
 		  dimension3: initialWidth
 		});
-	} else {
+	} else { 
 		ga("create", "UA-5828686-4", "auto");
-
-		// Secondary topics
-	    var dim6 = "";
-	    // Topic IDs
-	    var dim2 = "";
-
-	    // Google analytics doesn't accept arrays, these must be strings.
-	    if (window.PROJECT_ANALYTICS.secondaryTopics) {
-	      try {
-	        dim6 = window.PROJECT_ANALYTICS.secondaryTopics.join(", ");
-	      } catch (error) {
-	        console.log("PROJECT_ANALYTICS.secondaryTopics is not an array, check project.json");
-	      }
-	    }
-
-	    if (window.PROJECT_ANALYTICS.topicIDs) {
-	      try {
-	        dim2 = window.PROJECT_ANALYTICS.topicIDs.join(", ");
-	      } catch (error) {
-	        console.log("PROJECT_ANALYTICS.topicIDs is not an array, check project.json");
-	      }
-	    }
-
-	ga("set", {
-      dimension2: dim2,
-      dimension3: (window.PROJECT_ANALYTICS.primaryTopic || "News"),
-      dimension6: dim6,
-      dimension22: document.title
-	});
-	}
+		ga("set", {
+		  dimension2:  window.PROJECT_ANALYTICS.topicIDs,
+		  dimension3:  window.PROJECT_ANALYTICS.primaryTopic || "News",
+		  dimension6:  window.PROJECT_ANALYTICS.secondaryTopics || [],
+		  dimension22: document.title
+		});
+	} 
 	ga("send", "pageview");
 	googleAnalyticsAlreadyInitialized = true;
-}
+};
+
 
 // Add GA initialization to window.onload
 var oldOnload = window.onload;
@@ -89,4 +66,3 @@ document.addEventListener('npr:DataConsentChanged', () => {
     setupGoogleAnalytics();
   }  
 });
-
