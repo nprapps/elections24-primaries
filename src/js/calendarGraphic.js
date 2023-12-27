ec87b47e31191357121f504933ed3e2bf0ffdf25
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2023-12-26 17:25:46 -0800

    start styling buttons

--- src/js/calendarGraphic.js
+++ src/js/calendarGraphic.js
@@ -1,6 +1,7 @@
 const prev_button = document.querySelector("#cg__prev-button")
 const next_button = document.querySelector("#cg__next-button")
 const calendar_element = document.querySelector(".cg__viewport")
+const month_width = 185
 
 const today = new Date()
 // const current_month = today.getMonth()
@@ -8,7 +9,7 @@ const current_month = 0 // pretending it is january for testing
 let current_position
 
 const scroll_to_month = function(month) {
-	calendar_element.scroll(month * 165, 0)
+	calendar_element.scroll(month * month_width, 0)
 }
 
 const scroll_to_position = function(position) {
@@ -17,14 +18,14 @@ const scroll_to_position = function(position) {
 
 const scroll_to_previous_month = function() {
 	current_position = calendar_element.scrollLeft
-	scroll_to_position(current_position - 185)
+	scroll_to_position(current_position - month_width)
 }
 
 const scroll_to_next_month = function() {
 	current_position = calendar_element.scrollLeft
-	scroll_to_position(current_position + 185)
+	scroll_to_position(current_position + month_width)
 }
 
-scroll_to_month(current_month)
+// scroll_to_month(current_month)
 prev_button.addEventListener("click", scroll_to_previous_month)
 next_button.addEventListener("click", scroll_to_next_month)
