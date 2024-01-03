d1d03a65c7ea2cced6ebcb896c24c560a933e2d6
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-01-03 11:22:18 -0800

    Calendar (#58)
    * start 2024 calendar
    
    * remove 2020-specific widgets
    
    * add calendar graphic
    
    * style graphical version of calendar
    
    * color code event dates on calendar graphic
    
    * work with races data
    
    * format dates; remove a lot of unused code
    
    * style tabular calendar
    
    * beginning of calendar scroll functionality
    
    * start styling buttons
    
    * mobile styles; hide buttons when they are irrelevant
    
    * try z-indexing buttons
    
    * hover effects on desktop only; hide prev button on pageload (if january)
    
    * adjust calendar interactions for mobile and safari
    
    * color code squares by party
    
    * show super tuesday as a label
    
    * responsive calendar list styles

---
+++ src/js/calendarGraphic.js
@@ -0,0 +1,74 @@
+const prev_button = document.querySelector("#cg__prev-button")
+const next_button = document.querySelector("#cg__next-button")
+const calendar_element = document.querySelector(".cg__viewport")
+const visual_month_width = 165,
+  margin = 20,
+  month_width = visual_month_width + margin,
+  breakpoint = 1025,
+  scroll_width = month_width * 12 - margin
+const today = new Date()
+let calendar_element_width = calendar_element.getBoundingClientRect().width
+let current_month
+
+if (today.getFullYear() === 2024) {
+	current_month = today.getMonth()
+} else {
+	current_month = 0
+}
+
+const scroll_to_month = function(month) {
+	if (window.innerWidth > breakpoint) {
+		calendar_element.scroll(month * month_width, 0)
+	} else {
+		calendar_element.scroll(month * month_width - 60, 0)
+	}
+}
+
+const scroll_to_position = function(position) {
+	calendar_element.scroll(position, 0)
+}
+
+const recalculate_calendar_element_width = function() {
+	calendar_element_width = calendar_element.getBoundingClientRect().width
+}
+
+const scroll_to_previous_month = function() {
+	let position = calendar_element.scrollLeft
+	scroll_to_position(position - month_width)
+}
+
+const scroll_to_next_month = function() {
+	let position = calendar_element.scrollLeft
+	scroll_to_position(position + month_width)
+}
+
+const show_or_hide_buttons = function() {
+	let position = calendar_element.scrollLeft
+	if (position === 0) {
+		prev_button.classList.remove("shown")
+		prev_button.classList.add("hidden")
+		next_button.classList.remove("hidden")
+		next_button.classList.add("shown")
+	} else if (position === scroll_width - calendar_element_width) {
+		prev_button.classList.remove("hidden")
+		prev_button.classList.add("shown")
+		next_button.classList.remove("shown")
+		next_button.classList.add("hidden")
+	} else {
+		prev_button.classList.remove("hidden")
+		prev_button.classList.add("shown")
+		next_button.classList.remove("hidden")
+		next_button.classList.add("shown")
+	}
+}
+
+window.addEventListener("resize", recalculate_calendar_element_width)
+prev_button.addEventListener("click", scroll_to_previous_month)
+next_button.addEventListener("click", scroll_to_next_month)
+calendar_element.addEventListener("scroll", show_or_hide_buttons)
+
+scroll_to_month(current_month)
+if (current_month === 0) {
+		prev_button.classList.remove("shown")
+		prev_button.classList.add("hidden")
+}
