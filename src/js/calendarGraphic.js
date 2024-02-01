ee36dffe8b3650c80fe90f7172c203dd6c6d4d19
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-02-01 11:30:18 -0800

    highlight today's square - in JS this time! closes #116 (#117)

--- src/js/calendarGraphic.js
+++ src/js/calendarGraphic.js
@@ -15,6 +15,7 @@ if (today.getFullYear() === 2024) {
 } else {
 	current_month = 0
 }
+today_string = (today.getMonth() + 1) + "/" + today.getDate() + "/"  + today.getFullYear()
 
 const scroll_to_month = function(month) {
 	if (window.innerWidth > breakpoint) {
@@ -62,13 +63,17 @@ const show_or_hide_buttons = function() {
 	}
 }
 
-window.addEventListener("resize", recalculate_calendar_element_width)
-prev_button.addEventListener("click", scroll_to_previous_month)
-next_button.addEventListener("click", scroll_to_next_month)
-calendar_element.addEventListener("scroll", show_or_hide_buttons)
-
 scroll_to_month(current_month)
 if (current_month === 0) {
 		prev_button.classList.remove("shown")
 		prev_button.classList.add("hidden")
 }
+
+const squares = document.querySelectorAll(".calendar__square[data-date]")
+const today_square = [...squares].find(s => s.dataset.date === today_string)
+today_square.classList.add("today")
+
+window.addEventListener("resize", recalculate_calendar_element_width)
+prev_button.addEventListener("click", scroll_to_previous_month)
+next_button.addEventListener("click", scroll_to_next_month)
+calendar_element.addEventListener("scroll", show_or_hide_buttons)
