72e6b03c8840da8653f9a76700a9b1652f9d2199
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-02-07 14:23:08 -0800

    Cleanup (#128)
    * remove obsolete files
    
    * deleting more obsolete things

--- src/js/embeds/legacy-embeds.js
+++
@@ -1,9 +0,0 @@
-// load custom element shim
-var Sidechain = require("@nprapps/sidechain");
-var guest = Sidechain.registerGuest();
-
-// load the various result elements
-require("../components/president-primary");
-require("../components/iowa-widget");
-require("../components/nevada-widget");
-require("../components/delegate-total");
