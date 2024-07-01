fc9d4d84f492ce330108582922eac4b4f3741324
Brent Jones <bjones@npr.org>
2024-07-01 02:57:00 -0500

    adds GA4; closes #64 (#176)
    * test GA4
    
    * switch to ga4
    
    * fix variable names

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -2,22 +2,32 @@ var DataConsent = require('./lib/data-consent');
 var googleAnalyticsAlreadyInitialized = false;
 
 var setupGoogleAnalytics = function() {
+	if (window.top !== window) {
+		var gtagID = "G-LLLW9F9XPC" 
+	}
+	else 
+	{
+		var gtagID = "G-XK44GJHVBE" 
+	}
   // Bail early if opted out of Performance and Analytics consent groups
   if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) return;
 
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  var script = document.createElement("script")
 
-	if (window.top !== window) { 
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + gtagID
 
-		ga("create", "UA-5828686-75", "auto");
+  script.async = true;
+
+  var script_embed = document.createElement("script")
+
+  script_embed.innerHTML = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '" + gtagID + "');"
+
+  document.head.append(script, script_embed)
+
+if (window.top !== window) { 
+
 		// By default Google tracks the query string, but we want to ignore it.
 		var here = new URL(window.location);
-
-		ga("set", "location", here.protocol + "//" + here.hostname + here.pathname);
-		ga("set", "page", here.pathname);
 
 		// Custom dimensions & metrics
 		var parentUrl = here.searchParams.has("parentUrl") ? new URL(here.searchParams.get("parentUrl")) : "";
@@ -29,11 +39,13 @@ var setupGoogleAnalytics = function() {
 
 		var initialWidth = here.searchParams.get("initialWidth") || "";
 
-		ga("set", {
-		  dimension1: parentUrl,
-		  dimension2: parentHostname,
-		  dimension3: initialWidth
-		});
+		
+		var customData = {};
+        customData["dimension1"] = parentUrl;
+        customData["dimension2"] = parentHostname;
+        customData["dimension3"] = initialWidth;
+		gtag('config', gtagID, {'custom_map': {'dimension1': 'parentUrl', 'dimension2': 'parentHostname', 'dimension3': 'initialWidth'}});
+		gtag('event', 'page_view', customData)
 	} else { 
 
 		// Secondary topics
@@ -55,15 +67,18 @@ var setupGoogleAnalytics = function() {
 		  console.log("PROJECT_ANALYTICS.topicIDs is not an array, check project.json");
 		}
 
-		ga("create", "UA-5828686-4", "auto");
-		ga("set", {
-		  dimension2:  dim2,
-		  dimension3:  window.PROJECT_ANALYTICS.primaryTopic || "News",
-		  dimension6:  dim6,
-		  dimension22: document.title
-		});
-	} 
-	ga("send", "pageview");
+
+	var customData = {};
+        customData["dimension2"] = dim2;
+        customData["dimension3"] = window.PROJECT_ANALYTICS.primaryTopic || "News";
+        customData["dimension6"] = dim6;
+		customData["dimension22"] = document.title;
+	
+	// // gtag('set', 'send_page_view', false);
+    gtag('config', gtagID, {'custom_map': {'dimension2': '', 'dimension3': '', 'dimension6': '', 'dimension22': ''}});
+	}
+
+    gtag('event', 'page_view', customData)
 	googleAnalyticsAlreadyInitialized = true;
 };
 
